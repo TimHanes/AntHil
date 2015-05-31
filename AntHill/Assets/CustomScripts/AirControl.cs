f926c0aba87dbe34cc807c39a471c68a1b82c464
TimHanes <timofey-taliya@mail.ru>
2015-05-31 22:25:13 +0300

    Started Parting in ATOM

---
+++ AntHill/Assets/CustomScripts/AirControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirControl : MonoBehaviour {
+	public float Speed;
+	public float RotationSpeedRightLeft;
+	public float RotationSpeedUpDown;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (0, Speed, 0));
+		transform.Translate (new Vector3 (0, Input.GetAxis ("Vertical") * RotationSpeedRightLeft, 0));
+		transform.Translate (new Vector3 (Input.GetAxis ("Horizontal") * RotationSpeedRightLeft, 0, 0));
+	}
+}
