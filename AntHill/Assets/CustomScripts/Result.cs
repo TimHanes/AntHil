99eb1d982e1c0dadd69ae40a092df0ec2e25e38e
TimHanes <timofey-taliya@mail.ru>
2015-06-08 01:18:56 +0300

    Working version with burst soap Bobble.

---
+++ AntHill/Assets/CustomScripts/Result.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour {
+	public Text txt;
+	public static int Life = 5;
+	public int record = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Life == 0) {record = 0; Life = 5;}
+		int x;
+		x = (int) gameObject.transform.position.y;
+		if (x>record) record = x;
+		txt.text ="Hight = "+ x + "  Life = " + Life + "   Record = "+record;
+	}
+}
---
+++ AntHill/Assets/CustomScripts/StartBurst.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartBurst : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.renderer.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
